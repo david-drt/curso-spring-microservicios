fd8f7e509678bf3ba009fe1f3663f882d8050111
davidreytorr <davidreytorr@gmail.com>
2020-02-25 19:00:27 +0100

    crud

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceFeign.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceFeign.java
@@ -9,9 +9,10 @@ import org.springframework.stereotype.Service;
 
 import com.formacionbdi.springboot.app.item.clientes.ProductoClienteRest;
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 
 @Service("serviceFeign")
-@Primary
+
 public class ItemServiceFeign implements ItemService {
 
 	
@@ -28,6 +29,24 @@ public class ItemServiceFeign implements ItemService {
 	public Item findById(Long id, Integer cantidad) {
 	 
 		return new Item(clienteFeign.detalle(id),cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		// TODO Auto-generated method stub
+		return clienteFeign.crear(producto);
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+		// TODO Auto-generated method stub
+		return clienteFeign.update(producto, id);
+	}
+
+	@Override
+	public void delete(Long id) {
+		clienteFeign.eliminar(id);
+		
 	}
 
 }
--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -7,6 +7,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +18,7 @@ import com.formacionbdi.springboot.app.item.models.Item;
 import com.formacionbdi.springboot.app.item.models.Producto;
 
 @Service("serviceRestTemplate")
+@Primary
 public class ItemServiceImpl implements ItemService{
 
 	@Autowired
@@ -23,7 +28,7 @@ public class ItemServiceImpl implements ItemService{
 	public List<Item> findAll() {
 		List<Producto> productos = 
 				Arrays.asList(
-						clienteRest.getForObject("http://localhost:8001/listar", Producto[].class )); 
+						clienteRest.getForObject("http://servicio-productos/listar", Producto[].class )); 
 		return productos.stream().map(p -> new Item(p,1)).collect(Collectors.toList());
 	}
 
@@ -31,8 +36,35 @@ public class ItemServiceImpl implements ItemService{
 	public Item findById(Long id, Integer cantidad) {
 		Map<String,String> pathVariables = new HashMap<String,String>();
 		pathVariables.put("id",id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}",Producto.class, pathVariables);
+		Producto producto = clienteRest.getForObject("http://servicio-productos/ver/{id}",Producto.class, pathVariables);
 		return new Item(producto,cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		HttpEntity<Producto> body= new HttpEntity<>(producto);
+		ResponseEntity<Producto> response = clienteRest.exchange("http://servicio-productos/crear",HttpMethod. POST, body,Producto.class);
+		Producto productoResponse = response.getBody();
+		return productoResponse;
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+	 
+		HttpEntity<Producto> body= new HttpEntity<>(producto);
+		Map<String,String> pathVariables = new HashMap<String,String>();
+		pathVariables.put("id",id.toString());
+		ResponseEntity<Producto> response = clienteRest.exchange("http://servicio-productos/editar/{id}",HttpMethod. PUT, body,Producto.class,pathVariables );
+		Producto productoResponse = response.getBody();
+		return productoResponse;
+	}
+
+	@Override
+	public void delete(Long id) {
+		Map<String,String> pathVariables = new HashMap<String,String>();
+		pathVariables.put("id",id.toString());
+		 
+		clienteRest.delete("http://servicio-productos/eliminar/{id}",pathVariables);
 	}
 
 }
